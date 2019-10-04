1fabe4a62c745a46f0fb8909d80ff06585a1a653
NguyenThang <nguyen.thang@alliedtechcamp.com>
2019-10-04 09:58:33 +0700

    Add error handle

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,8 +5,9 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -88,7 +89,7 @@ class BurgerBuilder extends Component {
         this.setState({loading:true});
         const order = {
             ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
+            price: this.state.totalPrice.toFixed(2),
             customer: {
                 name: "Thang Nguyen",
                 address: {
@@ -100,7 +101,7 @@ class BurgerBuilder extends Component {
             },
             deliveryMethod: 'fastest'
         }
-        axios.post('/ordes.json', order).
+        axios.post('/orders.json', order).
             then(response => {
                 console.log(response);
                 this.setState({loading:false, purchasing: false});
@@ -131,6 +132,7 @@ class BurgerBuilder extends Component {
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     {orderSummary}
                 </Modal>
+                {withErrorHandler}
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
@@ -144,4 +146,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder,axios);
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Aux/Aux';
 
-const withErrorHandler = (WrapComponent) => {
-    return (props) => {
-        return (
-            <Aux>
-                <Modal>
-                    Something didn't work!.
-                </Modal>
-                <WrapComponent {...props} />
-            </Aux>
-        );
+const withErrorHandler = (WrapComponent, axios) => {
+    return class extends Component {
+        state = {
+            error: null,
+        }
+        componentDidMount() {
+            axios.interceptors.request.use(req => {
+                this.setState({ error: null });
+                return req;
+            });
+            axios.interceptors.response.use(res => res, error => {
+                this.setState({ error: error });
+            });
+        }
+
+        errorConfirmedHandler =() => {
+            this.setState({ error: null });
+        }
+        render() {
+            return (
+                <Aux>
+                    <Modal 
+                    show={this.state.error}
+                    modalClosed={this.errorConfirmedHandler}>
+                        {this.state.error ? this.state.error.message : null}
+                    </Modal>
+                    <WrapComponent {...this.props} />
+                </Aux>
+            );
+        }
     }
 
 }
